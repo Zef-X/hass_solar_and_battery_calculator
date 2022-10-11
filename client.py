f4d3ee75d97127b79e59d8c64f86f1bd211ebee0
Kilian Koch <kilian_koch@t-online.de>
2022-10-11 17:10:59 +0200

    fixed solar self-consumption
     - calculated a negative energy import instead of export
     - added net energy graph

--- client.py
+++ client.py
@@ -11,6 +11,8 @@ class client:
             "content-type": "application/json",
         }
 
+        self.SAMPLING_RATE = "5S"
+
     def get_data(self, sensors):
         real_data = pd.DataFrame(columns=sensors)
         for sensor in sensors:
@@ -31,12 +33,11 @@ class client:
 
         # cleanup file and remove all rows with value "unknown"
         real_data = real_data[real_data != "unknown"]
-
-        # resample the data to 15 seconds
-        real_data = real_data.ffill()
+        real_data = real_data[real_data != "unavailable"]
         real_data = real_data.astype(float)
-        real_data = real_data.resample("15S").mean()
-        real_data = real_data.round(2)
+
+        # correct data for original 5 second interval and interpolate missing values
+        real_data = real_data.resample(self.SAMPLING_RATE).nearest(limit=1).interpolate(method="linear")
 
         # rename the columns "Grid2Home", "Home2Grid" and "SolarProduction"
         real_data = real_data.rename(columns={"sensor.g2h_v6_power": "Grid2Home", "sensor.h2g_v6_power": "Home2Grid", "sensor.shelly1pm_244cab441f01_power": "SolarProduction"})
@@ -51,9 +52,10 @@ class client:
 
         return data
 
-    def simulate_data(self, data,current_pv_size, sim_pv_size):
+    def simulate_data_solar_only_old(self, data, current_pv_size, sim_pv_size):
         # normalize the data
         data["SolarProduction " + str(sim_pv_size) + "kWp"] = (data["SolarProduction"]/current_pv_size) * sim_pv_size
+        data["SolarProduction " + str(sim_pv_size) + "kWp"] = (data["SolarProduction"] / current_pv_size) * 1.5
         data["Additional_Solar_Production " + str(sim_pv_size) + "kWp"] = data["SolarProduction " + str(sim_pv_size) + "kWp"] - data["SolarProduction"]
 
         if data["Grid2Home"].sum() > 0:
@@ -74,9 +76,92 @@ class client:
 
         return data
 
+    def simulate_data_solar_only(self, data, current_pv_size, sim_pv_size):
+        # adjust production to pv_size
+        data["SolarProduction " + str(sim_pv_size) + "kWp"] = (data["SolarProduction"]/current_pv_size) * sim_pv_size
+        data["Additional_Solar_Production " + str(sim_pv_size) + "kWp"] = data["SolarProduction " + str(sim_pv_size) + "kWp"] - data["SolarProduction"]
+
+        # for every row, check if the additional solar production is higher than the grid2home
+        # if yes, then reduce the grid2home and increase the home2grid
+        data["Grid2Home " + str(sim_pv_size) + "kWp"] = data["Grid2Home"]
+        data["Home2Grid " + str(sim_pv_size) + "kWp"] = data["Home2Grid"]
+
+        for index, row in data.iterrows():
+            if row["Additional_Solar_Production " + str(sim_pv_size) + "kWp"] > row["Grid2Home"]:
+                additional_export = row["Additional_Solar_Production " + str(sim_pv_size) + "kWp"] - row["Grid2Home"]
+                reduced_import = row["Additional_Solar_Production " + str(sim_pv_size) + "kWp"] - additional_export
+
+                data.at[index, "Grid2Home " + str(sim_pv_size) + "kWp"] = row["Grid2Home"] - reduced_import
+                data.at[index, "Home2Grid " + str(sim_pv_size) + "kWp"] = row["Home2Grid"] + additional_export
+
+        # calculate power consumption as a checksum
+        data["HomeConsumption " + str(sim_pv_size) + "kWp"] = data["Grid2Home " + str(sim_pv_size) + "kWp"] + data["SolarProduction " + str(sim_pv_size) + "kWp"] - data["Home2Grid " + str(sim_pv_size) + "kWp"]
+
+        return data
+
+    def simulate_data_battery(self, data, current_pv_size, sim_pv_size, battery_size):
+        # check if there is data for sim_pv_size
+        if "SolarProduction " + str(sim_pv_size) + "kWp" not in data.columns:
+            data = self.simulate_data_solar_only(data, current_pv_size, sim_pv_size)
+
+        # simulate battery
+        Battery_SoC = "Battery_SoC " + str(sim_pv_size) + "kWp " + str(battery_size) + "kWh"
+        Battery_Charge = "Battery_Charge " + str(sim_pv_size) + "kWp " + str(battery_size) + "kWh"
+        Battery_Discharge = "Battery_Discharge " + str(sim_pv_size) + "kWp " + str(battery_size) + "kWh"
+        Battery_Efficiency = 0.9
+
+        data[Battery_SoC] = 0
+        data[Battery_Charge] = 0
+        data[Battery_Discharge] = 0
+
+        # if Export>0
+            # if SoC < BatterySize
+                #Charge Battery
+            # else
+                # Export
+
+        #df['equal_or_lower_than_4?'] = df['set_of_numbers'].apply(lambda x: 'True' if x <= 4 else 'False')
+        data[Battery_Charge] = data["Battery_SoC " + str(sim_pv_size) + "kWp " + str(battery_size) + "kWh"].apply(lambda x: 0 if x < 0 else x)
+
+    def simulate_data_solar_only_new(self, data, current_pv_size, sim_pv_size):
+        # adjust production to pv_size
+        sim_solar_production = (data["SolarProduction"]/current_pv_size) * sim_pv_size
+        sim_additional_solar_production = sim_solar_production - data["SolarProduction"]
+
+        # for every row, check if the additional solar production is higher than the grid2home
+        # if yes, then reduce the grid2home and increase the home2grid
+        sim_import = data["Grid2Home"]
+        sim_export = data["Home2Grid"]
+
+        for index, row in data.iterrows():
+            if (sim_additional_solar_production > row["Grid2Home"]):
+                sim_additional_export = sim_additional_solar_production - row["Grid2Home"]
+                sim_reduced_import = sim_additional_solar_production - additional_export
+
+                sim_import = row["Grid2Home"] - sim_reduced_import
+                sim_export = row["Home2Grid"] + sim_additional_export
+
+        # calculate power consumption as a checksum
+        sim_home_consumption = sim_import + sim_solar_production - sim_export
+
+        return data
+
+    def get_sum_import(self, data, pv_size):
+        return round(data["Grid2Home " + str(pv_size) + "kWp"].sum() / 720 / 1000, 2)
+
+    def get_sum_export(self, data, pv_size):
+        return round(data["Home2Grid " + str(pv_size) + "kWp"].sum() / 720 / 1000, 2)
+
+    def get_sum_consumption(self, data, pv_size):
+        return round(data["HomeConsumption " + str(pv_size) + "kWp"].sum() / 720 / 1000, 2)
+
+    def get_sum_solar_production(self, data, pv_size):
+        return round(data["SolarProduction " + str(pv_size) + "kWp"].sum() / 720 / 1000, 2)
+
     def calculate_solar_selfconsumption(self, data, pv_size):
-        # calculate the solar selfconsumption
-        summe_solar = data["SolarProduction " + str(pv_size) + "kWp"].sum()
-        summe_export = data["Home2Grid " + str(pv_size) + "kWp"].sum()
-        solar_selfconsumption = (summe_solar - summe_export) / (summe_solar)
-        return solar_selfconsumption
+        print("calculating self-consumption for " + str(pv_size) + "kWp")
+        return (self.get_sum_solar_production(data, pv_size) - self.get_sum_export(data, pv_size)) / self.get_sum_solar_production(data, pv_size)
+
+    def calculate_net_neutrality(self, data, pv_size):
+        print("calculating net neutrality for " + str(pv_size) + "kWp")
+        return self.get_sum_export(data, pv_size) / self.get_sum_import(data, pv_size)
--- main.py
+++ main.py
@@ -1,29 +1,80 @@
 # fetch data from "https://homeassistant.local:8123/" via the RESTful API
 import client
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import datetime
+
+# ----------------- CONFIGURATION -----------------
 
 # Home Assistant Token and URL
 TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1Mzc4YWUzMWFlMTI0MTZmOTYzNTNlNDM1ZWZjNzhkNSIsImlhdCI6MTY2NTMzODYwMywiZXhwIjoxOTgwNjk4NjAzfQ.UDjuxN_JZb5ddIA34ZY2hOOfdDWlskvpXrb6AeY4qIk"
 URL = "https://homeassistant.local:8123/"
 
 # Retrieve data since...
-FIRST_DATE = "2022-10-09T00:00:00.000Z"
+FIRST_DATE = "2022-10-09"
 
 # Sensors to retrieve - Grid2Home, Home2Grid, Solarproduction (Watt)
 SENSOR_NAMES = ["sensor.g2h_v6_power", "sensor.h2g_v6_power", "sensor.shelly1pm_244cab441f01_power"]
 
 # installed and max possible PV capacity
-CURRENT_PV_CAPACITY = 1.5
+CURRENT_PV_CAPACITY = 1.0
 MAX_PV_CAPACITY = 15.0
+
+
 
 if __name__ == '__main__':
     # get the data from the API
-    hass = client.client(URL, TOKEN, FIRST_DATE)
+    # convert FIRST_DATE to ISO format
+    start_date = datetime.datetime.strptime(FIRST_DATE, "%Y-%m-%d").isoformat() + "Z"
+    hass = client.client(URL, TOKEN, start_date)
+
+    # for every day since FIRST_DATE
+    first_date = datetime.date.fromisoformat(start_date[:10])
+    today = datetime.date.today()
+
+    # convert today to string "YYYY-MM-DDT00:00:00.000Z"
+    today_str = today.isoformat() + "T00:00:00.000Z"
+
+    # get the data from the API for every day and combine it into one dataframe
+    real_data = pd.DataFrame()
+    for day in range((today - first_date).days + 1):
+        date = (first_date + datetime.timedelta(days=day)).isoformat()
+        print("Fetching data for " + date)
+        # convert date to string "YYYY-MM-DDT00:00:00.000Z"
+        date_str = date + "T00:00:00.000Z"
+        hass = client.client(URL, TOKEN, date_str)
+        real_data = real_data.append(hass.get_data(SENSOR_NAMES))
+
+    '''
     real_data = hass.get_data(SENSOR_NAMES)
+    '''
+    print(real_data)
+
+    solar_selfconsumption = []
+    net_neutrality = []
 
     for pv_size in range(int(CURRENT_PV_CAPACITY), int(MAX_PV_CAPACITY)):
-        simulated_data = hass.simulate_data(real_data, CURRENT_PV_CAPACITY, pv_size)
+        simulated_data = hass.simulate_data_solar_only(real_data, CURRENT_PV_CAPACITY, pv_size)
 
-        print("solar_selfconsumption at " + str(pv_size) + "kWp :" + str(round(hass.calculate_solar_selfconsumption(simulated_data, pv_size), 4)))
+        solar_selfconsumption.append(hass.calculate_solar_selfconsumption(simulated_data, pv_size))
+        net_neutrality.append(hass.calculate_net_neutrality(simulated_data, pv_size))
+
+    #hass.simulate_data_battery(real_data,CURRENT_PV_CAPACITY, 8.0, 12.5)
+
+
+    # plot the results
+    plt.plot(range(int(CURRENT_PV_CAPACITY), int(MAX_PV_CAPACITY)), solar_selfconsumption)
+    plt.plot(range(int(CURRENT_PV_CAPACITY), int(MAX_PV_CAPACITY)), net_neutrality)
+    plt.legend(["Solar Selfconsumption", "Net Neutrality"])
+    plt.xlabel('PV size (kWp)')
+    plt.ylabel('percent')
+    plt.ylim(0, 1.5)
+    plt.show()
+    plt.savefig("graph.png")
+
+    solar_selfconsumption = pd.DataFrame(solar_selfconsumption)
+    solar_selfconsumption.to_csv("solar_selfconsumption.csv")
 
     simulated_data.to_csv("simulated_data.csv", encoding='utf-8', sep=';')
 
